62698974f5bba19334ac005a7c99b1a63b9fe521
Adrien <adrien.lachambre@bonitasoft.com>
2019-10-31 10:15:43 +0100

    update enum

--- src-groovy/ActivityState.groovy
+++ src-groovy/ActivityState.groovy
@@ -1,17 +1,7 @@
-
-enum ActivityState {
+public class ActivityState {
 	
-	REQUIRED("Required"), DISCRETIONARY("Discretionary"), NOT_AVAILABLE("N/A")
-	
-	private String name
-	
-	private ActivityState(String name) {
-		this.name = name
-	}
-	
-	@Override
-	public String toString() {
-		name
-	}
+	public static String REQUIRED = "Required"
+	public static String DISCRETIONARY = "Discretionary"
+	public static String NOT_AVAILABLE = "N/A"
 	
 }
